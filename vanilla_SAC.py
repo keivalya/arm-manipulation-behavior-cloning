4ae9a622ca782a5dcd36fc21da457fd641eb9ad4
keivalya <keivalyapandya2001@gmail.com>
2024-12-03 18:16:31 -0500

    changed vanilla for sac with data

--- vanilla_SAC.py
+++ vanilla_SAC.py
@@ -32,9 +32,13 @@ if __name__ == "__main__":
 
     memory = ReplayBuffer(replay_buffer_size, input_size=observation_size, n_actions=env.action_space.shape[0], augment_rewards=True, augment_data=True)
 
-    # memory.load_from_csv(filename=f'checkpoints/human_memory_{task_no_spaces}.npz')
-    # time.sleep(2)
+    memory.load_from_csv(filename=f'checkpoints/human_memory_{task_no_spaces}.npz')
+    time.sleep(2)
 
     # ZERO EXPERT DATA
-    memory.expert_data_ratio = 0.0
-    agent.train(env=env, memory=memory, episodes=1400, batch_size=batch_size, updates_per_step=updates_per_step, summary_writer_name=f"vanillaSAC_train_phase_1_{task_no_spaces}", max_episode_steps=max_episode_steps)
+    # memory.expert_data_ratio = 0.0
+    # agent.train(env=env, memory=memory, episodes=1400, batch_size=batch_size, updates_per_step=updates_per_step, summary_writer_name=f"vanillaSAC_train_phase_1_{task_no_spaces}", max_episode_steps=max_episode_steps)
+
+    # With all the data
+    memory.expert_data_ratio = 1.0
+    agent.train(env=env, memory=memory, episodes=1400, batch_size=batch_size, updates_per_step=updates_per_step, summary_writer_name=f"vanillaSAC_dataLoaded_train_phase_1_{task_no_spaces}", max_episode_steps=max_episode_steps)
